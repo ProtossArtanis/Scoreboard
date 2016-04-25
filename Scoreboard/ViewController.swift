58251071b72985cc9732edb1c258905fe7d92467
ProtossArtanis <steinsgate@mac24.local>
2016-04-25 21:22:54 +0800

    什么都没干啊
    什么都没干啊

--- Scoreboard/ViewController.swift
+++ Scoreboard/ViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBAction func neScore01(sender: AnyObject) {
+    }
+    @IBAction func neScore02(sender: AnyObject) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
